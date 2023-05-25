d5753ece1e72fbb30985a0fecd1c1a62bcece207
wprumizaca <wperezrumizaca@gmail.com>
2023-05-25 17:46:37 +0200

    Antes de apatacar a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListadoComponent } from './views/listado/listado.component';
+import { EntradaComponent } from './views/listado/entrada/entrada.component';
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListadoComponent,
+    EntradaComponent,
+    PaginaNoEncontradaComponent,
+    AcercaDeNosotrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
